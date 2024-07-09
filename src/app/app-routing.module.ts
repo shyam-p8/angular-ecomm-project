f51f1e6f802edfb71f9242bb37d0781144a70bc3
shyam-p8 <shyam.p8@gmail.com>
2024-07-09 17:26:17 +0530

    vedio 31 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { SellerAddProductComponent } from './seller-add-product/seller-add-produ
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
 import { SearchComponent } from './search/search.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 const routes: Routes = [
   {
@@ -41,6 +42,11 @@ const routes: Routes = [
   {
     path:'details/:productId',
     component:ProductDetailsComponent,
+    // canActivate:[AuthGuard]
+  },
+  {
+    path:'user-auth',
+    component:UserAuthComponent,
     // canActivate:[AuthGuard]
   }
 ];
